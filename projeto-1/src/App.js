cd7c0ea954a3fcd2cb0e5eaceece3fa24333f1a0
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-03-15 16:48:01 -0300

    misc componentDidMount example w timeout;

--- projeto-1/src/App.js
+++ projeto-1/src/App.js
@@ -1,43 +1,67 @@
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  const state = {
-    posts: [
-      {
-          id: 1,
-          title: 'Título 1',
-          body: 'Lorem1'
-      },
-      {
-          id: 2,
-          title: 'Título 2',
-          body: 'Lorem2'
-      },
-      {
-          id: 3,
-          title: 'Título 3',
-          body: 'Lorem3'
-      }
-    ]
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      posts: []
+    }
   }
 
-  const { posts } = state;
-
-  return (
-  <div>
-  {
-    posts.map( post =>
-      (<div key={post.id}>
-        <h1>
-          {post.title}
-        </h1>
-        <p>
-          {post.body}
-        </p>
-      </div>)
-      )
+  componentDidMount () {
+    this.setState({
+      posts: [
+        {
+          id: 0,
+          title: 'Componente Montado - aguardando API',
+          body: 'componentDidMount'
+        }
+      ]
+    });
+    setTimeout(() => {
+      this.setState({
+        posts: [
+          {
+            id: 1,
+            title: 'Título 1',
+            body: 'Lorem1'
+          },
+          {
+            id: 2,
+            title: 'Título 2',
+            body: 'Lorem2'
+          },
+          {
+            id: 3,
+            title: 'Título 3',
+            body: 'Lorem3'
+          }
+        ]
+      });
+    }, 5000);
   }
-  </div>)
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+    <div>
+    {
+      posts.map( post =>
+        (<div key={post.id}>
+          <h1>
+            {post.title}
+          </h1>
+          <p>
+            {post.body}
+          </p>
+        </div>)
+        )
+    }
+    </div>)
+  }
 }
 
 export default App;
