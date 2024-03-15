75c9832e2774bb841e9792098730467e3f3ce069
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-03-15 17:39:16 -0300

    misc fetch posts from jsonplaceholder;

--- projeto-1/src/App.js
+++ projeto-1/src/App.js
@@ -8,56 +8,30 @@ class App extends Component {
     this.state = {
       posts: [],
       counter: 0,
-      timeoutUpdate: null,
-
     };
   }
 
-  handleTimeout = () => {
-    this.timeoutUpdate = setTimeout(() => {
-      this.setState({
-        posts: [
-          {
-            id: 1,
-            title: 'Título 1',
-            body: 'Lorem1'
-          },
-          {
-            id: 2,
-            title: 'Título 2',
-            body: 'Lorem2'
-          },
-          {
-            id: 3,
-            title: 'Título 3',
-            body: 'Lorem3'
-          }
-        ],
-        counter: this.state.counter + 1,
-      });
-    }, 1000);
-    
+  getPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts => this.setState({ posts }));
   }
 
   componentDidMount () {
-    this.handleTimeout();
+    this.getPosts();
   }
 
   componentDidUpdate() {
-    clearTimeout(this.state.timeoutUpdate);
-    this.handleTimeout();
   }
 
   componentWillUnmount() {
-    clearTimeout(this.state.timeoutUpdate);
   }
 
   render() {
-    const { posts, counter } = this.state;
+    const { posts } = this.state;
 
     return (
     <div>
-    <h1>Contagem: {counter}</h1>
     {
       posts.map( post =>
         (<div key={post.id}>
