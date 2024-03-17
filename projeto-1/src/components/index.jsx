0ce40534f60a29ce7e73f7698bdcba79abc765c4
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-03-17 12:35:33 -0300

    misc clone component structure from PoC;

--- projeto-1/src/components/index.jsx
+++ projeto-1/src/components/index.jsx
@@ -1,4 +1,22 @@
-export default function PostCard() {
-    return(<>
-    </>)
+export default function PostCard(props) {
+    const { post } = props;
+
+    return (
+    <div className='post card'>
+        <img
+        className='post__img'
+        src={post.url}
+        alt={post.title}
+        />
+        <div
+        className='post__content'
+        >
+        <h1 className='post__title'>
+            {post.title}
+        </h1>
+        <p className='post__body'>
+            {post.body}
+        </p>
+        </div>
+    </div>)
 }
