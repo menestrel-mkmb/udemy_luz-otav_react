17e8f01f777013b5a6b538c990ba4a2dc04d9467
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-03-31 12:28:32 -0300

    fix naming convention to css files;

--- projeto-1/src/components/PostCard/index.jsx
+++ projeto-1/src/components/PostCard/index.jsx
@@ -1,4 +1,4 @@
-import './index.css';
+import './styles.css';
 
 export default function PostCard(props) {
     const { post } = props;
--- projeto-1/src/components/Posts/index.jsx
+++ projeto-1/src/components/Posts/index.jsx
@@ -1,4 +1,4 @@
-import './index.css';
+import './styles.css';
 
 import PostCard from '../PostCard';
 
