44215076dd7e39f28c0bc015a44a3d9daa9d3125
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-01 16:35:41 -0300

    misc posts on Posts Component instead of Home;

--- projeto-1/src/templates/Home/index.jsx
+++ projeto-1/src/templates/Home/index.jsx
@@ -1,44 +1,13 @@
 import './styles.css';
 import { Component } from 'react';
 
-import { loadPosts } from '../../utils/load-posts';
 import Posts from '../../components/Posts';
 
 class Home extends Component {
-
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    perPage: 10
-  };
-
-  getPosts = async () => {
-    const { page, perPage } = this.state;
-    
-    const completePosts = await loadPosts();
-
-    this.setState({
-      posts: completePosts.slice(page,
-                  page*perPage + perPage),
-      allPosts: completePosts
-    });
-  }
-
-  async componentDidMount () {
-    await this.getPosts();
-  }
-
   render() {
-    const { page, perPage, posts } = this.state;
-
     return (
       <section className='container posts__sect'>
-        <Posts
-          posts={posts}
-          page={page}
-          perPage={perPage}
-        />
+        <Posts />
       </section>)
   }
 }
