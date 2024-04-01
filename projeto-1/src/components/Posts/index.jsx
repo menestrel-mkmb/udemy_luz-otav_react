47373064a5ffbc279236d33201e5b0c742c94863
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-01 16:35:01 -0300

    clone Home state fn to Posts Component;

--- projeto-1/src/components/Posts/index.jsx
+++ projeto-1/src/components/Posts/index.jsx
@@ -1,16 +1,54 @@
 import './styles.css';
+import { Component } from 'react';
+
+import { loadPosts } from '../../utils/load-posts';
 
 import PostCard from '../PostCard';
+import Button from '../Button';
 
-const Posts = ({ posts }) => {
-    return(
-    <div className='posts wrapper'>
+class Posts extends Component {
+  state = {
+    posts: [],
+    allPosts: [],
+    page: 0,
+    perPage: 10
+  };
+
+  getPosts = async () => {
+    const { page, perPage } = this.state;
+    
+    const completePosts = await loadPosts();
+
+    this.setState({
+      posts: completePosts.slice(page,
+                  page*perPage + perPage),
+      allPosts: completePosts
+    });
+  }
+
+  async componentDidMount() {
+    return await this.getPosts();
+  }
+
+  loadMorePosts = () => {
+    console.log('loadMorePosts');
+  }
+
+  render() {
+    const { posts } = this.state;
+
+    return (
+    <article className='posts wrapper'>
       {
         posts.map( 
           post => <PostCard post={post} key={post.id} />)
       }
-    </div>
-    );
+      <Button
+        text={'Load more'}
+        onClick={this.loadMorePosts}
+      />
+    </article>
+  );}
 }
 
 export default Posts;
