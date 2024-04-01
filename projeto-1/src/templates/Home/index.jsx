98d70c1c2b0684296da313e94eae0358057754f6
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-01 15:25:58 -0300

    misc perPage n page for pagination;

--- projeto-1/src/templates/Home/index.jsx
+++ projeto-1/src/templates/Home/index.jsx
@@ -4,36 +4,43 @@ import { Component } from 'react';
 import { loadPosts } from '../../utils/load-posts';
 import Posts from '../../components/Posts';
 
-class Home extends Component {  
+class Home extends Component {
+
   state = {
     posts: [],
-    counter: 0,
+    allPosts: [],
+    page: 0,
+    perPage: 10
   };
 
   getPosts = async () => {
+    const { page, perPage } = this.state;
+    
     const completePosts = await loadPosts();
 
-    this.setState({ posts: completePosts });
+    this.setState({
+      posts: completePosts.slice(page,
+                  page*perPage + perPage),
+      allPosts: completePosts
+    });
   }
 
   async componentDidMount () {
     await this.getPosts();
   }
 
-  componentDidUpdate() {
-  }
-
-  componentWillUnmount() {
-  }
-
   render() {
-    const { posts } = this.state;
+    const { page, perPage, posts } = this.state;
 
     return (
       <section className='container posts__sect'>
-        <Posts posts={posts} />
+        <Posts
+          posts={posts}
+          page={page}
+          perPage={perPage}
+        />
       </section>)
   }
 }
   
-  export default Home;
+export default Home;
