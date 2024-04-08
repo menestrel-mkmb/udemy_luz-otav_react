fcce11a3e5e2d3ece4bdda0ed0a8b1c80914fc25
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-08 11:19:30 -0300

    misc using style on TextInput;

--- projeto-1/src/components/TextInput/index.jsx
+++ projeto-1/src/components/TextInput/index.jsx
@@ -1,6 +1,9 @@
+import "./styles.css"
+
 export default function TextInput({ searchValue, handleChange }) {
     return (
         <input
+            className="text__inp"
             type="search"
             placeholder="Search for post"
             onChange={handleChange}
