ca30ba9e7857407432797a2b9a7a21dfe36ae17d
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-09 19:54:00 -0300

    test fn event on btn click;

--- projeto-1/src/components/Button/Button.spec.jsx
+++ projeto-1/src/components/Button/Button.spec.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Button from '.';
 
@@ -6,7 +6,20 @@ describe('<Button /> See more posts', () => {
     it('should render the button with the text "Load more posts"', () => {
         expect.assertions(1);
         render(<Button text='Load more posts' />);
+
         const button = screen.getByRole('button', { name: /load more posts/i });
+
         expect(button).toBeInTheDocument();
     });
+
+    it('should call fn on button click', () => {
+        const fn = jest.fn();
+        expect.assertions(1);
+        render(<Button text='Load more posts' onClick={fn} />);
+
+        const button = screen.getByRole('button', { name: /load more posts/i });
+        fireEvent.click(button);
+        
+        expect(fn).toHaveBeenCalledTimes(1);
+    })
 });
