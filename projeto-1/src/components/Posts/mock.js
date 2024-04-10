44874ecc918b939957fb5ede5bdde47598df4556
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-09 21:30:22 -0300

    raw mock file for posts test;

---
+++ projeto-1/src/components/Posts/mock.js
@@ -0,0 +1,20 @@
+export const postsMockProps = [
+    {
+        id: 1,
+        title: 'Título 1',
+        body: 'Lorem1',
+        url: 'img/img1.jpg'
+    },
+    {
+        id: 2,
+        title: 'Título 2',
+        body: 'Lorem2',
+        url: 'img/img2.jpg'
+    },
+    {
+        id: 3,
+        title: 'Título 3',
+        body: 'Lorem3',
+        url: 'img/img3.jpg'
+    }
+];
