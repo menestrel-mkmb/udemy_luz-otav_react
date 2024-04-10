12e53889d12d142e07ace74bf01449e5f0e74d3a
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-10 10:53:45 -0300

    misc convert Home Page class to function;

--- projeto-1/src/templates/Home/index.jsx
+++ projeto-1/src/templates/Home/index.jsx
@@ -1,16 +1,13 @@
 import './styles.css';
-import { Component } from 'react';
 
 import Posts from '../../components/Posts';
 
-class Home extends Component {
-  render() {
-    return (
-      <section className='container posts__sect'>
-        <Posts posts={[]} />
-      </section>
-    );
-  }
+const Home = () => {
+  return (
+    <section className='container posts__sect'>
+      <Posts posts={[]} />
+    </section>
+  );
 }
 
 export default Home;
