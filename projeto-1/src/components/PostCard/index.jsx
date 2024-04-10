fbab5a2d97cb1b326ec5932787b0fab2a82023d0
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-09 21:04:34 -0300

    misc space adjust on PostCard;

--- projeto-1/src/components/PostCard/index.jsx
+++ projeto-1/src/components/PostCard/index.jsx
@@ -6,19 +6,23 @@ export default function PostCard(props) {
     return (
     <div className='post card'>
         <img
-        className='post__img'
-        src={post.url}
-        alt={post.title}
+            className='post__img'
+            src={post.url}
+            alt={post.title}
         />
         <div
-        className='post__content'
+            className='post__content'
         >
-        <h1 className='post__title'>
-            {post.title}
-        </h1>
-        <p className='post__body'>
-            {post.body}
-        </p>
+            <h1
+                className='post__title'
+            >
+                {post.title}
+            </h1>
+            <p
+                className='post__body'
+            >
+                {post.body}
+            </p>
         </div>
     </div>)
 }
