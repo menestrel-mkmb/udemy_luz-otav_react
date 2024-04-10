8da0633990d31b369e2f909be717bebc1f601e5a
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-09 21:31:16 -0300

    test Posts;

---
+++ projeto-1/src/components/Posts/Posts.spec.jsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import Posts from '.';
+import { postsMockProps } from './mock';
+
+const props = postsMockProps;
+
+describe('<Posts />', () => {
+    it('should render Posts', () => {
+        render(<Posts posts={props} />);
+
+        expect(screen.getAllByRole('heading', { name: /título/i }))
+            .toHaveLength(postsMockProps.length);
+        expect(screen.getAllByRole('img', { name: /título/i }))
+            .toHaveLength(postsMockProps.length);
+        expect(screen.getAllByText( /Lorem/i ))
+            .toHaveLength(postsMockProps.length);
+        expect(screen.getByRole('img', { name: /título 3/i }))
+            .toHaveAttribute('src', 'img/img3.jpg');
+    });
+
+    it('should match snapshot', () => {
+        const { container } = render(<Posts posts={props} />);
+        expect(container.firstChild).toMatchSnapshot();
+    })
+});
