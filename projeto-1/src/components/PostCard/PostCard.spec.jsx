a272768a7186bf4056d898018c000c0d8dc61fbb
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-09 21:04:56 -0300

    test PostCard;

---
+++ projeto-1/src/components/PostCard/PostCard.spec.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import PostCard from ".";
+import { postCardMockProps } from "./mock";
+
+const props = postCardMockProps;
+
+describe("<PostCard />", () => {
+    it('should render PostCard', () => {
+
+        render(<PostCard post={props} />);
+
+        expect(screen.getByRole('img', { name: 'Título 1' }))
+            .toHaveAttribute('src', 'img/img.jpg');
+        expect(screen.getByRole('img', { name: 'Título 1' }))
+            .toHaveAttribute('alt', 'Título 1');
+        expect(screen.getByRole('heading', { name: 'Título 1' }))
+            .toBeInTheDocument();
+        expect(screen.getByText('Lorem1'))
+            .toBeInTheDocument();
+    });
+});
