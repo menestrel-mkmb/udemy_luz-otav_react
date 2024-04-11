31366f33db62780e97da243c3783fbb96f094b1f
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-11 13:31:06 -0300

    misc FunctionApp for react useState;

--- projeto-2/src/App.js
+++ projeto-2/src/App.js
@@ -1,7 +1,24 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 
 import logo from './logo.svg';
 import './App.css';
+
+const FunctionApp = () => {
+  const [reverse, setReverse] = useState(false);
+
+  const handleClick = () => {
+    setReverse(!reverse);
+  };
+
+  return (
+    <div className="App">
+      <img src={logo} className={reverse ? 'App-logo reverse' : 'App-logo'} alt="logo" />
+      <button type="button" onClick={handleClick}>
+        Inverter giro
+      </button>
+    </div>
+  );
+};
 
 class ClassApp extends Component {
   constructor(props) {
@@ -35,6 +52,7 @@ function App() {
   return (
     <div className="App">
       <ClassApp />
+      <FunctionApp />
       <h1>Olá</h1>
     </div>
   );
