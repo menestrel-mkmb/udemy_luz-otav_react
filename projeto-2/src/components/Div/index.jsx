ac245d584800f84c170d6d379191da3e6dd691b9
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-11 23:01:28 -0300

    misc transform Div in ext Comp;

---
+++ projeto-2/src/components/Div/index.jsx
@@ -0,0 +1,12 @@
+import H1 from '../H1';
+import Body from '../Body';
+const Div = () => {
+  return (
+    <div className="div">
+      <H1 />
+      <Body />
+    </div>
+  );
+};
+
+export default Div;
