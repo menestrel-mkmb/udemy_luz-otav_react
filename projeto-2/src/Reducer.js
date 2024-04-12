2a93b9292c94fb748f692342a451fbe5698de5ef
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-12 10:26:15 -0300

    misc payload on reducer action;

--- projeto-2/src/Reducer.js
+++ projeto-2/src/Reducer.js
@@ -7,9 +7,14 @@ const reducer = (state, action) => {
       return { ...state, counter: state.counter + 1 };
     case 'decrement':
       return { ...state, counter: state.counter > 0 ? state.counter - 1 : 0 };
-    case 'toggleCaps':
-      return { ...state, title: state.title === 'OLÁ MUNDO' ? 'olá mundo' : 'OLÁ MUNDO' };
+    case 'toggleCaps': {
+      console.log(action.payload);
+      return { ...state, title: action.payload };
+    }
+    case 'invertBody':
+      return { ...state, body: state.body.split('').reverse().join('') };
   }
+  console.log('Nenhuma das ações anteriores foi executada');
   return { ...state };
 };
 
@@ -17,17 +22,19 @@ const ReducerApp = () => {
   const [state, dispatch] = useReducer(reducer, useContext(GlobalContext));
   const { counter, title, body } = state;
 
-  console.log(state, dispatch);
-  console.log(counter, title, body);
-
   return (
     <div className="App">
       <h2>
         {title} {counter}
       </h2>
+      <p>{body}</p>
       <button onClick={() => dispatch({ type: 'increment' })}>+</button>
       <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
-      <button onClick={() => dispatch({ type: 'toggleCaps' })}>Alterna caixa alta</button>
+      <button onClick={() => dispatch({ type: 'toggleCaps', payload: new Date().toLocaleString('pt-BR') })}>
+        Horário atual
+      </button>
+      <button onClick={() => dispatch({ type: 'invertBody' })}>Inverter texto</button>
+      <button onClick={() => dispatch({ type: 'ERROR' })}>Ação inexistente</button>
     </div>
   );
 };
