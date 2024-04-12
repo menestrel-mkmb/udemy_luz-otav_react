090709d93946161e823301d8547f1c1a6c80290c
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-11 22:56:11 -0300

    misc using context w diff States;

--- projeto-2/src/Context.js
+++ projeto-2/src/Context.js
@@ -1,32 +1,30 @@
 import { createContext, useContext, useState } from 'react';
 
 const GlobalContext = createContext();
-const globalState = {
-  title: 'Olá mundo',
-  counter: 0,
-  body: 'Lorem123',
-};
 
 const H1 = () => {
-  const {
-    contextState: {
-      globalState: { title },
-    },
-  } = useContext(GlobalContext);
-  return <h1 className="h1">{title}</h1>;
+  const { counter, title } = useContext(GlobalContext);
+  return (
+    <h1 className="h1">
+      {title} {counter}
+    </h1>
+  );
+};
+
+const Body = () => {
+  const { body, setCounter } = useContext(GlobalContext);
+  return (
+    <p className="p" onClick={() => setCounter((c) => c + 1)}>
+      {body}
+    </p>
+  );
 };
 
 const Div = () => {
-  const {
-    contextState: {
-      globalState: { body },
-    },
-  } = useContext(GlobalContext);
-
   return (
     <div className="div">
       <H1 />
-      <p className="p">{body}</p>
+      <Body />
     </div>
   );
 };
@@ -36,10 +34,13 @@ const ContextApp = () => {
 };
 
 const Context = () => {
-  const [contextState, setContextState] = useState({ globalState });
+  const [counter, setCounter] = useState(0);
+  const [title, setTitle] = useState('Olá mundo');
+  const [body, setBody] = useState('Olá mundo');
+
   return (
     <div className="App">
-      <GlobalContext.Provider value={{ contextState, setContextState }}>
+      <GlobalContext.Provider value={{ counter, setCounter, title, setTitle, body, setBody }}>
         <ContextApp />
       </GlobalContext.Provider>
     </div>
