358a7138335c3eabca6bbabf1730ae6f9fe431ae
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-14 12:51:14 -0300

    misc CounterErrorBoundary Component;

---
+++ projeto-3/src/components/CounterErrorBoundary/index.jsx
@@ -0,0 +1,30 @@
+import { Component } from 'react';
+
+class CounterErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Atualiza o state para que a próxima renderização mostre a UI alternativa.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Você também pode registrar o erro em um serviço de relatórios de erro
+    // console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Você pode renderizar qualquer UI alternativa
+      return <p>Deu ruim =(</p>;
+    }
+
+    // eslint-disable-next-line react/prop-types
+    return this.props.children;
+  }
+}
+
+export default CounterErrorBoundary;
