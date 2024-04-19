afb19ce27644d68d3e19ee3ce022c847515ae07c
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-18 22:42:06 -0300

    misc using CounterContext Provider on app;

--- react-context-api-and-hooks/src/index.js
+++ react-context-api-and-hooks/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './styles/global-styles.css';
 
 import { Home } from './templates/Home';
+import { CounterContextProvider } from './contexts/CounterContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <CounterContextProvider>
+      <Home />
+    </CounterContextProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
