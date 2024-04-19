34a1c6f3650025f4f4b4c9d49c2b2d876bba9e3b
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-18 22:43:05 -0300

    misc testing dispatch build action;

--- react-context-api-and-hooks/src/templates/Home/index.jsx
+++ react-context-api-and-hooks/src/templates/Home/index.jsx
@@ -1,7 +1,12 @@
+import { useCounterContext } from '../../contexts/CounterContext';
+
 export const Home = () => {
+  const [state, action] = useCounterContext();
+
   return (
     <div>
-      <h1>Oi</h1>
+      <h1 onClick={() => action.increment()}>Oi</h1>
+      <h1>{state.counter}</h1>
     </div>
   );
 };
