97c0832cea9818707dbcca8b4c90bf737449c68f
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-18 22:04:36 -0300

    misc actions enum;

---
+++ react-context-api-and-hooks/src/contexts/CounterContext/actions-types.js
@@ -0,0 +1,9 @@
+const prefix = 'CounterContext';
+
+export const INCREMENT = `${prefix}/INCREMENT`;
+export const DECREMENT = `${prefix}/DECREMENT`;
+export const RESET = `${prefix}/RESET`;
+export const SET_COUNTER = `${prefix}/SET_COUNTER`;
+export const ASYNC_INCREMENT_START = `${prefix}/ASYNC_INCREMENT_START`;
+export const ASYNC_INCREMENT_END = `${prefix}/ASYNC_INCREMENT_END`;
+export const ASYNC_INCREMENT_ERROR = `${prefix}/ASYNC_INCREMENT_ERROR`;
