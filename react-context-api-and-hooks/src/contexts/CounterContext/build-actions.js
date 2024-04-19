1915e9ef05c3623f2b7d785b0f49e5804de2b407
Michael Kevin <56491958+menestrel-mkmb@users.noreply.github.com>
2024-04-18 23:57:33 -0300

    misc CounterContext n Hooks async dispatch;

--- react-context-api-and-hooks/src/contexts/CounterContext/build-actions.js
+++ react-context-api-and-hooks/src/contexts/CounterContext/build-actions.js
@@ -5,8 +5,28 @@ export const buildActions = (dispatch) => {
     decrement: () => dispatch({ type: actionTypes.DECREMENT }),
     reset: () => dispatch({ type: actionTypes.RESET }),
     setCounter: (payload) => dispatch({ type: actionTypes.SET_COUNTER, payload }),
-    asyncIncrementStart: () => dispatch({ type: actionTypes.ASYNC_INCREMENT_START }),
-    asyncIncrementEnd: () => dispatch({ type: actionTypes.ASYNC_INCREMENT_END }),
-    asyncIncrementError: () => dispatch({ type: actionTypes.ASYNC_INCREMENT_ERROR }),
+    asyncIncrementStart: () => asyncIncStart(dispatch),
+    asyncIncrementError: () => asyncIncErrorExample(dispatch),
   };
+};
+const asyncIncStart = async (dispatch) => {
+  dispatch({ type: actionTypes.ASYNC_INCREMENT_START });
+
+  return await new Promise((resolve) => {
+    setTimeout(() => {
+      dispatch({ type: actionTypes.ASYNC_INCREMENT_END });
+      resolve('Resolved');
+    }, 2000);
+  });
+};
+
+const asyncIncErrorExample = async (dispatch) => {
+  dispatch({ type: actionTypes.ASYNC_INCREMENT_START });
+
+  return await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      dispatch({ type: actionTypes.ASYNC_INCREMENT_ERROR });
+      reject('Rejected');
+    }, 2000);
+  });
 };
